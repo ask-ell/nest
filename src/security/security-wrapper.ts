f48a0b3e5d9d8769c59f5ff86970bc2e9816cf55
Yannick Debrée <yannickdebree@ask-ell.com>
2025-05-08 15:58:28 +0200

    nestjs/common per dependency created

--- src/security/security-wrapper.ts
+++ src/security/security-wrapper.ts
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import csurf from "csurf";
 
 export class SecurityWrapper {
-  static wrap(application: INestApplication): void {
+  static wrap<T>(application: INestApplication<T>): void {
     application.use(cookieParser());
     application.use(csurf({ cookie: true }));
   }
